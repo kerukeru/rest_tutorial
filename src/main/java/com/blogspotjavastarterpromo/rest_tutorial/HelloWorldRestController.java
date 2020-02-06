68913277f500b58049852cb7794dc1f8a7440fa8
keru <keru@protonmail.com>
2020-02-06 18:45:23 +0100

    3.1

---
+++ src/main/java/com/blogspotjavastarterpromo/rest_tutorial/HelloWorldRestController.java
@@ -0,0 +1,16 @@
+package com.blogspotjavastarterpromo.rest_tutorial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldRestController {
+
+    @GetMapping("/api/hello")
+    public Greeting hello(){
+        Greeting gr = new Greeting();
+        gr.setMsg("hello!");
+        return gr;
+    }
+
+}
