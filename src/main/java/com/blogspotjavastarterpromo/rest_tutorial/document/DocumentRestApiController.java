160b2861c2d2836e6a25c6d4921879cf173a498a
keru <keru@protonmail.com>
2020-02-11 14:34:18 +0100

    3.4 - pobieranie zasobow done

--- src/main/java/com/blogspotjavastarterpromo/rest_tutorial/document/DocumentRestApiController.java
+++ src/main/java/com/blogspotjavastarterpromo/rest_tutorial/document/DocumentRestApiController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/documents")
@@ -14,8 +15,19 @@ public class DocumentRestApiController {
     private Collection<Document> documents = new ArrayList<>();
 
     @GetMapping
-    public Iterable<Document> getDocument(){
+    public Iterable<Document> getDocuments(){
         return documents;
+    }
+
+    @GetMapping("/{number}")
+    public Optional<Document> getDocument(@PathVariable long number){
+        return documents.stream().filter(doc -> doc.getNumber()==number).findAny();
+    }
+
+    @GetMapping("/{number}/title")
+    public Optional<String> getTitle(@PathVariable long number){
+        return documents.stream().filter(doc -> doc.getNumber()==number)
+                .findAny().map(Document::getTitle);
     }
 
     @PostMapping
@@ -25,7 +37,8 @@ public class DocumentRestApiController {
 
     @PostMapping(value = "/{docNumber}/tags", consumes = MediaType.TEXT_PLAIN_VALUE)
     public void addTag(@PathVariable long docNumber,@RequestBody String tag){
-        documents.stream().filter(document -> document.getNumber() == docNumber)
+        documents.stream()
+                .filter(document -> document.getNumber() == docNumber)
                 .findAny().ifPresent(document -> document.getTags().add(tag));
 
     }
